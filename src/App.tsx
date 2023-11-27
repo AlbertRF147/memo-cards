f51817ccad9f1d5427255e10ebb16eba17253c5d
albertrf147 <albert@netenders.com>
2023-11-27 18:59:18 +0100

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import './App.css'
 import AppProvider from './context/appContext'
-import { Board } from './containers/Board'
+import { Board } from './components/Board'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is the game popup</div>,
+  },
+  {
+    path: "/game",
+    element: <Board />
+  }
+])
 
 function App() {
 
   return (
     <AppProvider>
       <main className='app'>
-        <Board />
+        <RouterProvider router={router} />
       </main>
     </AppProvider>
   )
